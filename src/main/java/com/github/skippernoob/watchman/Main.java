2186cacf9d76e50489f6152238bc6d35bb55aa9f
SkipperNoob <hristyukoleksandr@gmail.com>
2015-11-17 15:36:44 +0200

    add main and test

---
+++ src/main/java/com/github/skippernoob/watchman/Main.java
@@ -0,0 +1,7 @@
+package com.github.skippernoob.watchman;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Watchman project will born here soon");
+    }
+}
---
+++ src/test/java/com/github/skippernoob/watchman/SampleTest.java
@@ -0,0 +1,11 @@
+package com.github.skippernoob.watchman;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class SampleTest {
+    @Test
+    public void testMath() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
